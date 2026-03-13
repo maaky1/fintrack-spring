83f2d0884965351d62ee9df79cd0825b2c17cc9a
F <f.idevs@pm.me>
2026-03-13 15:34:44 +0700

    Add new dev

--- src/main/java/org/maaky1/fintrack/service/UserService.java
+++ src/main/java/org/maaky1/fintrack/service/UserService.java
@@ -2,6 +2,9 @@ package org.maaky1.fintrack.service;
 
 import org.maaky1.fintrack.entity.UserEntity;
 import org.maaky1.fintrack.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -10,11 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    public void getByUsername(String username) {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity result = userRepository.findByUsername(username);
         log.info("{}", result);
+        return null;
     }
 }
