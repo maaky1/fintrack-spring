42f691dc3dfc80768a529f81294789c84e335d4c
F <f.idevs@pm.me>
2026-04-11 18:00:56 +0700

    Add swagger

--- src/main/java/org/maaky1/fintrack/configuration/security/WebSecurityConfig.java
+++ src/main/java/org/maaky1/fintrack/configuration/security/WebSecurityConfig.java
@@ -34,7 +34,12 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(ex -> ex
